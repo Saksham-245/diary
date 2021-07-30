17006c2c257832cfb99b0c8b4019b687ed836e88
Saksham Mathur <saksham.mathur25@gmail.com>
2021-07-30 21:29:50 +0530

    Added netlify redirect configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import React, { useState } from "react";
-import {
-  BrowserRouter,
-  HashRouter,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import "./App.css";
@@ -17,7 +11,6 @@ function App() {
         <Switch>
           <Route path="/" exact component={Login} />
           <Route path="/dashboard" component={Dashboard} />
-          <Redirect from="*" to={Dashboard} />
         </Switch>
       </BrowserRouter>
     </div>
