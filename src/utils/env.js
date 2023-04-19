1c828dde51aa97562f7848296898e71c752b282d
Saksham <saksham.mathur25@gmail.com>
2023-04-20 02:29:54 +0530

    added env variables

---
+++ src/utils/env.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL='https://cyhjzyoezhdlqybgaldk.supabase.co'
+export const SUPABASE_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMTkyMjMyMSwiZXhwIjoxOTM3NDk4MzIxfQ.haVNnBl8b-52vLVHTK4vjdOIhH8LHjGUwM8wz7SKsJg'
+export const QUOTE_API_KEY = 'vZCw4L/8gSfm9bLEvePjkg==kbIB78nd8gCtfrvL';
